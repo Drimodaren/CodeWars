bd43bca9d0fe699d6821ae77a454db26442878c9
Drimodaren <men.a.a@yandex.ru>
2022-12-09 15:07:19 +0400

    Map

--- toJadenCase.js
+++ toJadenCase.js
@@ -1,8 +1,8 @@
-// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). 
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013).
 // Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
 //  For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 
-// Your task is to convert strings to how they would be written by Jaden Smith. 
+// Your task is to convert strings to how they would be written by Jaden Smith.
 // The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
 
 // Example:
@@ -27,3 +27,12 @@ const toJadenCase = (str) => {
 console.log(toJadenCase("How can mirrors be real if our eyes aren't real"));
 console.log(toJadenCase('When I die. Then You Will Realize'));
 console.log(toJadenCase('most Trees Are Blue'));
+
+const toJadenCaseMap = (str) => {
+  return str
+    .split(' ')
+    .map((world) => world.charAt(0).toUpperCase() + world.slice(1))
+    .join(' ');
+};
+console.log(toJadenCaseMap("How can mirrors be real if our eyes aren't real"));
+console.log(toJadenCaseMap('most Trees Are Blue'));
